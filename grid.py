658dfc049b9d32ee06a69a089fbb5427ca8cf874
Rafaelsc12 <168587013+Rafaelsc12@users.noreply.github.com>
2024-05-13 17:02:54 -0300

    Update grid.py
    Otimizada no código
    Adição de linhas verticais e horizontais

--- grid.py
+++ grid.py
@@ -1,34 +1,30 @@
 import pygame
-
+from colors import Colors
 class Grid:
-    def __init__ (self):
-        self.linhas = 22
-        self.colunas = 11
-        self.cell_tamanho = 33
-        self.grid = [[0 for i in range (self.colunas)]for j in range(self.linhas)]
-        self.cores = self.cor_das_malhas()
+    def __init__(self):
+        self.num_linhas = 20
+        self.num_colunas = 10
+        self.cell_tamanho = 30 
+        self.grid = [[0 for i in range(self.num_colunas)] for j in range(self.num_linhas)]
+        self.cores = Colors.cor_das_malhas()
 
     def print_grid(self):
-        for linha in range(self.linhas):
-            for coluna in range(self.colunas):
-                print(self.grid[linha][coluna], end = ' ') 
-            print ()
-
-    def cor_das_malhas (self):
-        cinza_escuro = (26, 31, 40)
-        verde = (47, 230, 23)
-        vermelho = (232, 18, 18)
-        laranja = (226, 116, 15)
-        amarelo = (237, 233, 4)
-        roxo = (166, 0, 247)
-        azul_ciano = (21, 204, 209)
-        azul = (17, 65, 216)
-    
-        return [cinza_escuro, verde, vermelho, laranja, amarelo, roxo, azul_ciano, azul]
-
-    def pintura(self, tela):
-        for linha in range(self.linhas):
-            for coluna in range(self.colunas):
+        for linha in range(self.num_linhas):
+            for coluna in range(self.num_colunas):
+                print(self.grid[linha][coluna], end=" ") 
+            print()
+    def draw(self, screen):
+        #Espaços do grid
+        for linha in range(self.num_linhas):
+            for coluna in range(self.num_colunas):
                 valor_malha = self.grid[linha][coluna]
                 malha_hit = pygame.Rect(coluna*self.cell_tamanho + 1, linha*self.cell_tamanho + 1, self.cell_tamanho - 1, self.cell_tamanho)
-                pygame.draw.rect(tela, self.cores[valor_malha], malha_hit)
+                pygame.draw.rect(screen, self.cores[valor_malha], malha_hit)
+        
+        # Linhas horizontais
+        for linha in range(self.num_linhas + 1):
+            pygame.draw.line(screen, (100, 0, 100), (0, linha*self.cell_tamanho), (self.num_colunas*self.cell_tamanho, linha*self.cell_tamanho), 1)
+        
+        # Linhas verticais
+        for coluna in range(self.num_colunas + 1):
+            pygame.draw.line(screen, (100, 0, 100), (coluna*self.cell_tamanho, 0), (coluna*self.cell_tamanho, self.num_linhas*self.cell_tamanho), 1)
