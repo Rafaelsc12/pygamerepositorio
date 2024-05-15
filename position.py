299c1d69d6d83d9ebfa3e2940addd2b7e2d154c1
David Chung <davidyhc@al.insper.edu.br>
2024-05-15 18:00:07 -0300

    Código da posição que esqueci de colocar

--- position.py
+++ position.py
@@ -1,4 +1,22 @@
-class Position:
-	def __init__(self, linha, coluna):
-		self.linha = linha
-		self.coluna = coluna
+class Posicao:
+    def __init__(self, linha, coluna):
+        self.linha = linha
+        self.coluna = coluna
+
+    def mover(self, delta_linha, delta_coluna):
+        self.linha += delta_linha
+        self.coluna += delta_coluna
+
+    def copiar(self):
+        return Posicao(self.linha, self.coluna)
+
+    def _eq_(self, other):
+        if isinstance(other, Posicao):
+            return self.linha == other.linha and self.coluna == other.coluna
+        return False
+
+    def _repr_(self):
+        return f"Posicao(linha={self.linha}, coluna={self.coluna})"
+
+    def _hash_(self):
+        return hash((self.linha, self.coluna))
